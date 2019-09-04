360f744391d94a731077cce755ab360939980429
ebroadbent <broadbent52@aol.com>
2019-09-04 10:07:58 -0600

    example projects first push

---
+++ example_projects/src/ericsb52_example_projects/Hello_world.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package ericsb52_example_projects;
+
+/**
+ * @author ebroadbent
+ * 9/19
+ * hello world
+ */
+public class Hello_world {
+
+	/**
+	 * 
+	 */
+	public Hello_world() {
+		System.out.println("hello world");
+		System.out.println("im sick of hello world");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		new Hello_world();
+		
+
+	}
+
+}
